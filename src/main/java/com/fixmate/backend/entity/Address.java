be30a4176afade26a7416df5d2de34e5b796dc20
Thisharu Dulaksha <thisharud@gmail.com>
2025-11-30 17:56:43 +0530

    update application.yml

---
+++ src/main/java/com/fixmate/backend/entity/Address.java
@@ -0,0 +1,31 @@
+package com.fixmate.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "table")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long addressId;
+
+    private String address;
+    private String city;
+
+    private Double latitude;
+    private Double longitude;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id",nullable = false)
+    private User user;
+
+
+}
