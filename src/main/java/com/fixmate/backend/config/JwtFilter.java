3c77e66bfbeafc115daa7c16316f323184e56a4c
Jdilsham <janithadilsham@gmail.com>
2025-12-05 23:12:28 +0530

    fix security config

--- src/main/java/com/fixmate/backend/config/JwtFilter.java
+++ src/main/java/com/fixmate/backend/config/JwtFilter.java
@@ -40,6 +40,16 @@ public class JwtFilter extends OncePerRequestFilter {
             FilterChain chain
     ) throws ServletException, IOException {
 
+        res.setHeader("Access-Control-Allow-Origin", "http://35.200.239.169");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "*");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
+            res.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+
         String header = req.getHeader("Authorization");
 
         String token = null;
