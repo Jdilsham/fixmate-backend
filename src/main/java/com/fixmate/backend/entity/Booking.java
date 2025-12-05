e5628a0a4d942f6ebdf9245c6f4db4ba4d793be0
Thisharu Dulaksha <thisharud@gmail.com>
2025-12-05 18:38:45 +0530

    Add Booking entity

--- src/main/java/com/fixmate/backend/entity/Booking.java
+++ src/main/java/com/fixmate/backend/entity/Booking.java
@@ -1,4 +1,42 @@
 package com.fixmate.backend.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Entity
+@Table(name = "booking")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+
+    @Column(name = "total_price")
+    private BigDecimal totalPrice;
+
+    @Column(name = "scheduled_at")
+    private Instant scheduledAt;
+
+    @Column(name = "created_at")
+    private Instant createdAt = Instant.now();
+
+    private String status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToOne(mappedBy = "booking",cascade = CascadeType.ALL, orphanRemoval = true)
+    private Payment payment;
+
 }
