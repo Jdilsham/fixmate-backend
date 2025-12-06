741b533d128ca32efacdbc47e27d810934ef6ac4
Sasitha <sasitha01jay@gmail.com>
2025-12-06 12:54:03 +0530

    Update User entity

--- src/main/java/com/fixmate/backend/entity/User.java
+++ src/main/java/com/fixmate/backend/entity/User.java
@@ -50,6 +50,12 @@ public class User {
     @OneToMany(mappedBy = "user",  cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Booking> bookings = new HashSet<>();
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<ServiceProvider> serviceProviders = new HashSet<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Review> reviews = new HashSet<>();
+
     public User(String firstname, String lastname, String email, String phone, String password,  String role) {
         this.firstName = firstname;
         this.lastName = lastname;
