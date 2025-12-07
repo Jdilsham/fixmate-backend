352ab52170091e8e53c54ce485de0cdfb785e6b7
Sasitha <sasitha01jay@gmail.com>
2025-12-07 11:19:01 +0530

    Add Review entity

---
+++ src/main/java/com/fixmate/backend/entity/Review.java
@@ -0,0 +1,41 @@
+package com.fixmate.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Entity
+@Table(name = "review")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ReviewId;
+
+    private BigDecimal rating;
+    private String comment;
+
+    @Column(name = "created_at")
+    private Instant createdAt = Instant.now();
+
+    @OneToOne
+    @JoinColumn(name = "booking_id")
+    private Booking booking;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "service_provider_id")
+    private ServiceProvider serviceProvider;
+
+}
