cf9f2d72019c264a0578fbc78871a61ca6bba1c5
Jdilsham <janithadilsham@gmail.com>
2025-12-14 00:13:02 +0530

    add

--- src/main/resources/db/migration/V4__add_booking_id_to_address.sql
+++
@@ -1,9 +0,0 @@
--- Add booking_id column to address table
-ALTER TABLE address
-    ADD COLUMN booking_id BIGINT;
-
--- Add foreign key constraint
-ALTER TABLE address
-    ADD CONSTRAINT fk_address_booking
-        FOREIGN KEY (booking_id)
-            REFERENCES booking(booking_id);
