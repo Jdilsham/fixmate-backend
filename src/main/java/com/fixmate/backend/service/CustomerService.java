8db977c92362f83712eae6dc116dd7607ff7840c
Thisharu Dulaksha <thisharud@gmail.com>
2025-12-27 19:24:51 +0530

    create getProfile method

--- src/main/java/com/fixmate/backend/service/CustomerService.java
+++ src/main/java/com/fixmate/backend/service/CustomerService.java
@@ -1,4 +1,7 @@
 package com.fixmate.backend.service;
 
+import com.fixmate.backend.dto.response.CustomerProfileResponse;
+
 public interface CustomerService {
+    CustomerProfileResponse getProfile(Long customerId);
 }
