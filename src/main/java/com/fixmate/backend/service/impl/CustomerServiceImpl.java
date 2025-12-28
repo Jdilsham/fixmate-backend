05409cf5bc140bd630a262f613949b39dd5eae29
Thisharu Dulaksha <thisharud@gmail.com>
2025-12-28 11:46:27 +0530

    feat: implement getProfile method logic

--- src/main/java/com/fixmate/backend/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/fixmate/backend/service/impl/CustomerServiceImpl.java
@@ -1,4 +1,40 @@
 package com.fixmate.backend.service.impl;
 
-public class CustomerServiceImpl {
+import com.fixmate.backend.dto.response.CustomerProfileResponse;
+import com.fixmate.backend.entity.Role;
+import com.fixmate.backend.entity.User;
+import com.fixmate.backend.mapper.CustomerMapper;
+import com.fixmate.backend.repository.UserRepository;
+import com.fixmate.backend.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import static org.springframework.http.HttpStatus.*;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerServiceImpl  implements CustomerService {
+    private final UserRepository userRepository;
+    private final CustomerMapper mapper;
+
+    @Override
+    @Transactional(readOnly = true)
+    public CustomerProfileResponse getProfile(String email) {
+       User user = getUserByEmail(email);
+       return mapper.toProfileResponse(user);
+    }
+
+
+    //Ensure user exists and is a Customer
+    private User getUserByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND,"User not found"));
+    }
+
+
 }
+
+
