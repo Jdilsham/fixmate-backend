8cb9f2576bda9495061e6f2f9d075f6e18ef8156
Thisharu Dulaksha <thisharud@gmail.com>
2025-12-28 12:27:05 +0530

    feat(mapper): add update mapping method using @MappingTarget

--- src/main/java/com/fixmate/backend/mapper/CustomerMapper.java
+++ src/main/java/com/fixmate/backend/mapper/CustomerMapper.java
@@ -1,10 +1,14 @@
 package com.fixmate.backend.mapper;
 
+import com.fixmate.backend.dto.request.CustomerUpdateReq;
 import com.fixmate.backend.dto.response.CustomerProfileResponse;
 import com.fixmate.backend.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface CustomerMapper {
     CustomerProfileResponse toProfileResponse(User user);
+
+    void updateCustomerFromReq(CustomerUpdateReq req, @MappingTarget User user);
 }
