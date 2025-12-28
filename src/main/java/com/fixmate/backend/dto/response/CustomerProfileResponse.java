a78a65f55d67cbd24963e3ceee7df39ded0c5d3e
Thisharu Dulaksha <thisharud@gmail.com>
2025-12-28 11:09:12 +0530

    update dto

--- src/main/java/com/fixmate/backend/dto/response/CustomerProfileResponse.java
+++ src/main/java/com/fixmate/backend/dto/response/CustomerProfileResponse.java
@@ -1,16 +1,16 @@
 package com.fixmate.backend.dto.response;
 
 import com.fixmate.backend.entity.Address;
+import lombok.Data;
 
 import java.time.Instant;
 import java.util.Set;
 
+@Data
 public class CustomerProfileResponse {
     private Long id;
     private String firstName;
     private String lastName;
     private String email;
     private String phone;
-    private Instant createsAt;
-    private Set<AddressResponse> addresses;
 }
