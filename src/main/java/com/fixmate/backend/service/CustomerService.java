bded93c8f45c80313a7807913842ca645025d55d
Thisharu Dulaksha <thisharud@gmail.com>
2025-12-28 12:47:10 +0530

    feat: add customer update method

--- src/main/java/com/fixmate/backend/service/CustomerService.java
+++ src/main/java/com/fixmate/backend/service/CustomerService.java
@@ -1,7 +1,10 @@
 package com.fixmate.backend.service;
 
+import com.fixmate.backend.dto.request.CustomerUpdateReq;
 import com.fixmate.backend.dto.response.CustomerProfileResponse;
 
 public interface CustomerService {
     CustomerProfileResponse getProfile(String email);
+
+    CustomerProfileResponse updateProfile(String email, CustomerUpdateReq req);
 }
