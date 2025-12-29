827ad580876576978292413b84b2fea57644b4c0
Thisharu Dulaksha <thisharud@gmail.com>
2025-12-29 18:39:23 +0530

    add BookingRequest class

---
+++ src/main/java/com/fixmate/backend/dto/request/BookingRequest.java
@@ -0,0 +1,27 @@
+package com.fixmate.backend.dto.request;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class BookingRequest {
+
+    @NotNull
+    private Long serviceId;
+
+    @NotNull
+    private Long providerId;
+
+    @NotNull
+    private Long addressId;
+
+    @Future
+    @NotNull
+    private LocalDateTime scheduledTime;
+
+
+
+}
