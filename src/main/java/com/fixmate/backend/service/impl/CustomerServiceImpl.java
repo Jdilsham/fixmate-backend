57630c91d0091cd9c63fa18b42a62bbee427c20e
Thisharu Dulaksha <thisharud@gmail.com>
2025-12-29 18:45:57 +0530

    feat: implement  createBooking method logic

--- src/main/java/com/fixmate/backend/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/fixmate/backend/service/impl/CustomerServiceImpl.java
@@ -1,15 +1,23 @@
 package com.fixmate.backend.service.impl;
 
+import com.fixmate.backend.dto.request.BookingRequest;
 import com.fixmate.backend.dto.request.CustomerUpdateReq;
+import com.fixmate.backend.dto.response.BookingResponse;
 import com.fixmate.backend.dto.response.CustomerProfileResponse;
+import com.fixmate.backend.entity.Booking;
 import com.fixmate.backend.entity.User;
+import com.fixmate.backend.enums.BookingStatus;
+import com.fixmate.backend.enums.Role;
 import com.fixmate.backend.mapper.CustomerMapper;
+import com.fixmate.backend.repository.BookingRepository;
 import com.fixmate.backend.repository.UserRepository;
 import com.fixmate.backend.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.time.ZoneId;
 
 import static org.springframework.http.HttpStatus.*;
 
@@ -18,6 +26,7 @@ import static org.springframework.http.HttpStatus.*;
 public class CustomerServiceImpl  implements CustomerService {
     private final UserRepository userRepository;
     private final CustomerMapper mapper;
+    private final BookingRepository bookingRepository;
 
     @Override
     @Transactional(readOnly = true)
@@ -31,6 +40,22 @@ public class CustomerServiceImpl  implements CustomerService {
         User user = getUserByEmail(email);
         mapper.updateCustomerFromReq(req,user);
         return mapper.toProfileResponse(userRepository.save(user));
+    }
+
+    @Override
+    public BookingResponse createBooking(String email, BookingRequest req){
+        User user = getUserByEmail(email);
+
+        if (user.getRole() != Role.CUSTOMER) {
+            throw new ResponseStatusException(FORBIDDEN, "Only customers can create bookings");
+        }
+
+        Booking booking = new Booking();
+        booking.setUser(user);
+        booking.setScheduledAt(req.getScheduledTime().atZone(ZoneId.systemDefault()).toInstant());
+        booking.setStatus(BookingStatus.PENDING);
+
+        return mapper.toBookingResponse(bookingRepository.save(booking));
     }
 
 
