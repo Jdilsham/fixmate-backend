0c20f14ec8fd945c7539e33c3fa63a4af06f04dd
Thisharu Dulaksha <thisharud@gmail.com>
2025-12-29 18:45:00 +0530

    feat: add createBooking method

--- src/main/java/com/fixmate/backend/service/CustomerService.java
+++ src/main/java/com/fixmate/backend/service/CustomerService.java
@@ -1,10 +1,14 @@
 package com.fixmate.backend.service;
 
+import com.fixmate.backend.dto.request.BookingRequest;
 import com.fixmate.backend.dto.request.CustomerUpdateReq;
+import com.fixmate.backend.dto.response.BookingResponse;
 import com.fixmate.backend.dto.response.CustomerProfileResponse;
 
 public interface CustomerService {
     CustomerProfileResponse getProfile(String email);
 
     CustomerProfileResponse updateProfile(String email, CustomerUpdateReq req);
+
+    BookingResponse createBooking(String email, BookingRequest request);
 }
