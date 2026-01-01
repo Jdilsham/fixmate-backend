971dcb4db67e89efd08636d5eeca36873cac5f95
Thisharu Dulaksha <thisharud@gmail.com>
2026-01-01 21:46:49 +0530

    feat add: ProviderSearchResponse class

---
+++ src/main/java/com/fixmate/backend/dto/response/ProviderSearchResponse.java
@@ -0,0 +1,9 @@
+package com.fixmate.backend.dto.response;
+
+public class ProviderSearchResponse {
+    private Long providerId;
+    private String name;
+    private String serviceType;
+    private String city;
+    private Double rating;
+}
