8dbf939a26048ce0666776b0775abea8f5964314
Thisharu Dulaksha <thisharud@gmail.com>
2026-01-03 01:21:54 +0530

    add BookingResponse dto

--- src/main/java/com/fixmate/backend/dto/response/BookingResponse.java
+++ src/main/java/com/fixmate/backend/dto/response/BookingResponse.java
@@ -1,11 +1,13 @@
 package com.fixmate.backend.dto.response;
 
 import com.fixmate.backend.enums.BookingStatus;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
 public class BookingResponse {
     private Long bookingId;
     private BookingStatus status;
