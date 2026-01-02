1d6e888ba7f6b4d2875c76dae0e7917db38bea01
Thisharu Dulaksha <thisharud@gmail.com>
2026-01-02 23:35:21 +0530

    update

--- src/main/java/com/fixmate/backend/dto/response/BookingResponse.java
+++ src/main/java/com/fixmate/backend/dto/response/BookingResponse.java
@@ -1,5 +1,6 @@
 package com.fixmate.backend.dto.response;
 
+import com.fixmate.backend.enums.BookingStatus;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -7,11 +8,6 @@ import java.time.LocalDateTime;
 @Data
 public class BookingResponse {
     private Long bookingId;
-    private String status;
-    private double price;
-    private String providerName;
-    private String serviceName;
-    private LocalDateTime scheduledAt;
-
+    private BookingStatus status;
 
 }
