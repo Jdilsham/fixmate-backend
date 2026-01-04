98e43cc1f1535aca80c88232cd7b3790411ec841
Sasitha <sasitha01jay@gmail.com>
2026-01-04 09:56:33 +0530

    added getProfile and getEarnings

---
+++ src/main/java/com/fixmate/backend/service/ServiceProviderService.java
@@ -0,0 +1,22 @@
+package com.fixmate.backend.service;
+
+import com.fixmate.backend.dto.request.ProfileUpdateReq;
+import com.fixmate.backend.dto.response.*;
+import com.fixmate.backend.entity.ServiceProvider;
+
+import java.util.List;
+
+public interface ServiceProviderService {
+
+    ProviderProfileDTO getProfile(Long userId);
+
+    void updateProfile(Long userId, ProfileUpdateReq req);
+
+    void requestVerification(Long userId);
+
+    List<BookingDetailResponse> getBookings(Long userId);
+
+    EarningSummaryDTO getEarnings(Long userId);
+
+    ServiceProvider getVerifiedProviderByUserId(Long userId);
+}
