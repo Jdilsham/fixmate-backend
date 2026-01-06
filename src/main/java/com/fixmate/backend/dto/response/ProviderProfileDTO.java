b426512843cc3e657930776a90eea604c9235809
Sasitha <sasitha01jay@gmail.com>
2026-01-06 15:57:44 +0530

    add some dtos

--- src/main/java/com/fixmate/backend/dto/response/ProviderProfileDTO.java
+++ src/main/java/com/fixmate/backend/dto/response/ProviderProfileDTO.java
@@ -1,12 +1,24 @@
 package com.fixmate.backend.dto.response;
 
+import lombok.Builder;
 import lombok.Data;
 import java.math.BigDecimal;
+import java.util.List;
+
 
 @Data
+@Builder
 public class ProviderProfileDTO {
+    private Long providerId;
     private String fullName;
+    private String description;
     private String skill;
     private BigDecimal rating;
     private Boolean isVerified;
+    private Boolean isAvailable;
+    private String profileImage;
+    private String city;
+
+    private List<ServiceResponseDTO> services;
+    private Boolean isOwner;
 }
