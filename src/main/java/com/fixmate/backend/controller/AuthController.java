51375d4f45a39b0801dbe6107f0288d71fb07a78
Thisharu Dulaksha <thisharud@gmail.com>
2026-01-14 21:29:59 +0530

    feat: implement user signup with email verification logic and endpoints

--- src/main/java/com/fixmate/backend/controller/AuthController.java
+++ src/main/java/com/fixmate/backend/controller/AuthController.java
@@ -1,11 +1,12 @@
 package com.fixmate.backend.controller;
 
+import com.fixmate.backend.dto.request.EmailVerifyRequest;
 import com.fixmate.backend.dto.request.GoogleLoginRequest;
-import com.fixmate.backend.dto.response.AuthResponse;
 import com.fixmate.backend.dto.request.LoginRequest;
 import com.fixmate.backend.dto.request.SignupRequest;
 import com.fixmate.backend.service.AuthService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,35 +15,53 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/auth")
 @CrossOrigin(origins = "http://localhost:5173")
-
+@RequiredArgsConstructor
 public class AuthController {
 
-    private final AuthService auth;
-
-    public AuthController(AuthService auth) {
-        this.auth = auth;
-    }
+    private final AuthService authService;
 
     @PostMapping("/signup")
-    public ResponseEntity<?> signup(@Valid @RequestBody SignupRequest r) {
-        auth.signup(r);
-        return ResponseEntity.ok("User Created");
+    public ResponseEntity<String> signup(
+            @Valid @RequestBody SignupRequest request
+    ) {
+        authService.signup(request);
+        return ResponseEntity.ok(
+                "Signup successful. Please verify your email."
+        );
     }
 
     @PostMapping("/login")
-    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest r) {
-        String token = auth.login(r);
-        return ResponseEntity.ok(new AuthResponse(token));
+    public ResponseEntity<?> login(
+            @Valid @RequestBody LoginRequest request
+    ) {
+        return ResponseEntity.ok(
+                authService.login(request)
+        );
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<String> verify(
+            @Valid @RequestBody EmailVerifyRequest request
+    ) {
+        authService.verifyUser(
+                request.getEmail(),
+                request.getCode()
+        );
+        return ResponseEntity.ok(
+                "Email verified successfully"
+        );
     }
 
     @PostMapping("/google")
-    public ResponseEntity<?> googleLogin(
-            @RequestBody GoogleLoginRequest request
+    public ResponseEntity<Map<String, String>> googleLogin(
+            @Valid @RequestBody GoogleLoginRequest request
     ) {
-        String token = auth.googleLogin(request.getIdToken());
+        String token = authService.googleLogin(
+                request.getIdToken()
+        );
+
         return ResponseEntity.ok(
                 Map.of("token", token)
         );
     }
-
 }
